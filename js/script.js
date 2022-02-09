7a5b350f56f8173995001a07dbbcb6002c91ce90
ilyalogonow <ilyalogonow@gmail.com>
2022-02-09 23:49:45 +0300

    finished layout and connected animation

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({
+    reset: true,
+    duration: 2500,
+    distance: '151px',
+    delay: 300
+});
+
+    sr.reveal('.custom-clothing', {
+        origin: 'left',
+         
+    }); 
