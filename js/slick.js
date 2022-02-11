e850a59c48678ef6b8270bda2a1f2fe114f9c865
ilyalogonow <ilyalogonow@gmail.com>
2022-02-11 16:02:18 +0300

    The first version of the mobile layout

--- js/slick.js
+++ js/slick.js
@@ -4,4 +4,29 @@ $('.slider').slick({
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1240,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 840,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
